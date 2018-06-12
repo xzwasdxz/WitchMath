66046756da8c33e1f58c51ff959c3e76aa1337d6
Sachkov Yuri <sachkovura2011@gmail.com>
2018-06-12 15:49:23 +0300

    release v1.1

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -4,20 +4,21 @@ int main() {
 	ofstream tau2("foo.txt");
 	ofstream l3("foooo.txt");
 	ofstream tau5("fooo.txt");
-	size_t max_amount = 100;
-	double t_step = 0.0001;
-	double x_step = 0.0001;
+	size_t max_amount = 87;
+	double tau = 2.5;
 	size_t n_x_steps = 10000;
 	size_t n_t_steps = 10000;
+	double t_step = 1/n_t_steps;
+	double x_step = 1/n_x_steps;
 	double* sqrt_lambda = new double[max_amount];
 	sqrt_lambda = roots(max_amount, 1, sqrt_lambda); // searching lambdas
 	double u = 0;
-	double* utau2 = new double[n_x_steps]; // plot for t=2*tau assuming tau=1,l=1 
-	thread t1 (it1, n_x_steps, x_step, sqrt_lambda, max_amount, utau2);
+	double* utau2 = new double[n_x_steps]; // plot for t=2*tau assuming l=1 
+	thread t1 (it1, n_x_steps, x_step, sqrt_lambda, max_amount, utau2, tau);
 	double* utau5 = new double[n_x_steps];
-	thread t2 (it2, n_x_steps, x_step, sqrt_lambda, max_amount, utau5);// plot for t=5*tau assuming tau=1,l=1 
-	double* ul3 = new double[5 * n_t_steps];
-	thread t3 (it3, n_t_steps, t_step, sqrt_lambda, max_amount, ul3);// plot for x=l/3 assuming tau=1,l=1 
+	thread t2 (it2, n_x_steps, x_step, sqrt_lambda, max_amount, utau5, tau);// plot for t=5*tau assuming l=1 
+	double* ul3 = new double[5 * n_t_steps * tau];
+	thread t3 (it3, n_t_steps, t_step, sqrt_lambda, max_amount, ul3, tau);// plot for x=l/3 assuming l=1 
 	t1.join();
 	t2.join();
 	t3.join();
--- Project2/threads.h
+++ Project2/threads.h
@@ -1,22 +1,22 @@
 #pragma once //approximation of the solution by dots and push it to files.
 #include"headers.h"
-double* it1(double n_x_steps, double x_step, double* sqrt_lambda, int64_t max_amount, double* tau2) {
-	for (int i = 0; i < n_x_steps; ++i) {
-		double u = i * x_step - 2 + sum(sqrt_lambda, max_amount, i*x_step, 2., 1., 1.);
+double* it1(double n_x_steps, double x_step, double* sqrt_lambda, int64_t max_amount, double* tau2, double tau) {
+	for (size_t i = 0; i < n_x_steps; ++i) {
+		double u = i * x_step - 2 + sum(sqrt_lambda, max_amount, i*x_step, 2.*tau, 1., tau);
 		tau2[i] = u;
 	}
 	return tau2;
 }
-double* it2(double n_x_steps, double x_step, double* sqrt_lambda, int64_t max_amount, double* tau5) {
-	for (int i = 0; i < n_x_steps; ++i) {
-		double u = i * x_step - 2 + sum(sqrt_lambda, max_amount, i * x_step, 5., 1., 1.);
+double* it2(double n_x_steps, double x_step, double* sqrt_lambda, int64_t max_amount, double* tau5, double tau) {
+	for (size_t i = 0; i < n_x_steps; ++i) {
+		double u = i * x_step - 2 + sum(sqrt_lambda, max_amount, i * x_step, 5.*tau, 1., tau);
 		tau5[i] = u;
 	}
 	return tau5;
 }
-double* it3(double n_t_steps, double t_step, double* sqrt_lambda, int64_t max_amount, double* l3) {
-	for (int i = 0; i < 5 * n_t_steps; ++i) {
-		double u = 1 / 3 - 2 + sum(sqrt_lambda, max_amount, 1 / 3, i * t_step, 1., 1.);
+double* it3(double n_t_steps, double t_step, double* sqrt_lambda, int64_t max_amount, double* l3, double tau) {
+	for (size_t i = 0; i < 5 * n_t_steps * tau; ++i) {
+		double u = 1 / 3 - 2 + sum(sqrt_lambda, max_amount, 1 / 3, i * t_step, 1., tau);
 		l3[i] = u;
 	}
 	return l3;
